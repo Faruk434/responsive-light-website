55d1a6ef501c4a65bfe2c34249c9296075aa8194
Faruk434 <63547363+Faruk434@users.noreply.github.com>
2020-07-14 14:35:12 +0600

    Added html, css files

---
+++ js/myScript.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+   // sticky top menu js
+    
+    $(window).scroll(function(){
+        var scrolling = $(this).scrollTop();
+        var sticky = $('.sticky-top');
+        
+        if(scrolling >= 200){
+            sticky.addClass('nav-bg');
+        }
+        else{
+            sticky.removeClass('nav-bg');
+        }
+    });
+    
+    
+//    wowjs
+    new WOW().init();
+    
+});
